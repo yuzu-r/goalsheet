bd74228d3eee3bee2fbe856826804ed613fbc180
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-09-15 21:37:40 +0000

    hook up goal create and create controller tests

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,4 +1,5 @@
 class GoalsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @goals = Goal.all
   end
@@ -7,4 +8,17 @@ class GoalsController < ApplicationController
     @goal = Goal.new
   end
 
+  def create
+    @goal = current_user.goals.create(goal_params)
+    if @goal.valid?
+      redirect_to root_path
+    else
+      render :new, {:user_id => u.id}, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def goal_params()
+      params.require(:goal).permit(:title, :description, :frequency, :session_length)
+    end
 end
--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -12,13 +12,26 @@ RSpec.describe GoalsController, type: :controller do
     it "successfully shows the new goal form" do
       u = FactoryGirl.create(:user)
       sign_in u
-      get :new, {:user_id => u.id}
+      get :new, user_id: u.id
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'goals#create' do
-    it "requires user to be logged in"
-    it "succesfully creates a new goal in the database"
+    it "requires user to be logged in" do
+      goal_count = Goal.count
+      post :create, user_id: 1, goal: { title: "fail!" }
+      expect(response).to redirect_to new_user_session_path
+      expect(Goal.count).to eq goal_count
+    end
+    it "succesfully creates a new goal in the database" do
+      u = FactoryGirl.create(:user)
+      sign_in u
+      post :create, user_id: u.id, goal: { title: "my goal" }
+      expect(response).to redirect_to root_path
+      goal = Goal.last
+      expect(goal.title).to eq("my goal")
+      expect(goal.user).to eq(u)
+    end
   end
 end
