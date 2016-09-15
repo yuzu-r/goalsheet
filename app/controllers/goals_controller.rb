b40422ffd83729112932183f851025f924ec009d
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-09-15 06:06:14 +0000

    new goal form

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -2,4 +2,9 @@ class GoalsController < ApplicationController
   def index
     @goals = Goal.all
   end
+
+  def new
+    @goal = Goal.new
+  end
+
 end
--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe GoalsController, type: :controller do
+  describe 'goals#index' do
+    it "successfully shows the index page" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
 
+  describe 'goals#new' do
+    it "successfully shows the new goal form" do
+      u = FactoryGirl.create(:user)
+      sign_in u
+      get :new, {:user_id => u.id}
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'goals#create' do
+    it "requires user to be logged in"
+    it "succesfully creates a new goal in the database"
+  end
 end
