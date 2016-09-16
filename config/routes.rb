ed42a71870aa646160ab5fd3729094e6358fce5e
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-09-16 00:12:34 +0000

    create placeholder dashboard and welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'goals#index'
+  root 'static_pages#welcome'
   resources :users do 
     resources :goals, only: [:new, :create, :edit, :destroy, :index]
   end
+  resource :dashboard, only: [:show]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
