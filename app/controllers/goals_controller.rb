5be6a0e5d3f48e4f460a042658a44de388e24627
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-09-29 22:48:52 +0000

    edit/delete goals; plans for deleted goal also deleted

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,5 +1,5 @@
 class GoalsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :index]
+  before_action :authenticate_user!, only: [:new, :create, :index, :edit, :update, :destroy]
   def index
     @goals = Goal.where(user: current_user)
   end
@@ -17,7 +17,32 @@ class GoalsController < ApplicationController
     end
   end
 
+  def edit
+    @goal = Goal.find(params[:id])
+  end
+
+  def update
+    current_goal.update_attributes(goal_params)
+    if current_goal.valid?
+      redirect_to user_goals_path(current_user)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if current_goal.user_id != current_user.id
+      return render text: "Not Allowed!", status: :forbidden
+    end
+    current_goal.destroy
+    redirect_to user_goals_path(current_user)
+  end
+
   private
+    def current_goal
+      @goal || Goal.find(params[:id])
+    end
+
     def goal_params()
       params.require(:goal).permit(:title, :description, :frequency, :session_length)
     end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,5 @@
 class Goal < ActiveRecord::Base
-  belongs_to :plan
+  has_many :plans, dependent: :destroy
   belongs_to :user
   validates :title, presence: true
   validates :user_id, presence: true
